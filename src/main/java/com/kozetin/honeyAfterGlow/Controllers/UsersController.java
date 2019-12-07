dd67df508c86ccb9edb49d319db51290489408cd
kozetin <kozetin@yandex.ru>
2019-12-07 18:55:29 +0300

    controller for user editing form added

--- src/main/java/com/kozetin/honeyAfterGlow/Controllers/UsersController.java
+++ src/main/java/com/kozetin/honeyAfterGlow/Controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.kozetin.honeyAfterGlow.Controllers;
 
+import com.kozetin.honeyAfterGlow.Domain.Role;
+import com.kozetin.honeyAfterGlow.Domain.User;
 import com.kozetin.honeyAfterGlow.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,13 @@ public class UsersController {
         model.addAttribute("userslist",userRepository.findAll());
 
         return "users";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user",user);
+        model.addAttribute("roles", Role.values());
+
+        return "userEdit";
     }
 }
