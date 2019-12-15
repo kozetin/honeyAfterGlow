45494db0eb3f0eb6d17b8f4f3aaf069e576a46e1
kozetin <kozetin@yandex.ru>
2019-12-15 09:52:53 +0300

    product model created

---
+++ src/main/java/com/kozetin/honeyAfterGlow/Domain/Product.java
@@ -0,0 +1,20 @@
+package com.kozetin.honeyAfterGlow.Domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String imageSource;
+    private String type;
+    private Integer price;
+    private Set<String> tags;
+    
+}
