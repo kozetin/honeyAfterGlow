8df9bc1196f58f6a50a7a02f5d177cf231491d7e
kozetin <kozetin@yandex.ru>
2019-12-26 22:15:54 +0300

    ProductController getmapping logic added

--- src/main/java/com/kozetin/honeyAfterGlow/Controllers/ProductController.java
+++ src/main/java/com/kozetin/honeyAfterGlow/Controllers/ProductController.java
@@ -2,9 +2,14 @@ package com.kozetin.honeyAfterGlow.Controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @PreAuthorize("hasAuthority('ADMIN')")
 public class ProductController {
-    
+
+    @GetMapping("/addproduct")
+    public String addProduct() {
+        return "addproduct";
+    }
 }
