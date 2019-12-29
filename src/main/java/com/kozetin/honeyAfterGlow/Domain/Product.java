cd53dd8cb4c1cf15170dce9903e619ff8bdbae67
kozetin <kozetin@yandex.ru>
2019-12-29 12:59:48 +0300

    getters and setters added

--- src/main/java/com/kozetin/honeyAfterGlow/Domain/Product.java
+++ src/main/java/com/kozetin/honeyAfterGlow/Domain/Product.java
@@ -17,4 +17,59 @@ public class Product {
     private Integer price;
     private Set<String> tags;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    public void setImageSource(String imageSource) {
+        this.imageSource = imageSource;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
 }
