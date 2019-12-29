e203c4532860250b43dfa6c3227a77db8c63ae22
kozetin <kozetin@yandex.ru>
2019-12-29 14:09:43 +0300

    addproduct logic added

--- src/main/java/com/kozetin/honeyAfterGlow/Controllers/ProductController.java
+++ src/main/java/com/kozetin/honeyAfterGlow/Controllers/ProductController.java
@@ -1,15 +1,37 @@
 package com.kozetin.honeyAfterGlow.Controllers;
 
+import com.kozetin.honeyAfterGlow.Domain.Product;
+import com.kozetin.honeyAfterGlow.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @PreAuthorize("hasAuthority('ADMIN')")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
 
     @GetMapping("/addproduct")
     public String addProduct() {
         return "addproduct";
+    }
+
+    @PostMapping("/addproduct")
+    public String addProduct(Product product, Model model, @RequestParam("file") MultipartFile file) {
+        String addProductMessage = productService.addProduct(product, file);
+        if (!addProductMessage.equals("OK")) {
+            model.addAttribute("message",addProductMessage);
+            return "addproduct";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/kozetin/honeyAfterGlow/Service/ProductService.java
+++ src/main/java/com/kozetin/honeyAfterGlow/Service/ProductService.java
@@ -1,4 +1,63 @@
 package com.kozetin.honeyAfterGlow.Service;
 
+import com.kozetin.honeyAfterGlow.Domain.Product;
+import com.kozetin.honeyAfterGlow.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    public String addProduct(Product product, MultipartFile file) {
+
+        //product name validation
+        if (product.getName().isEmpty() || product.getName() == null) {
+            return "Введите значение в поле Название товара";
+        }
+
+        //productDescription validation
+        if (product.getDescription().isEmpty() || product.getDescription() == null) {
+            return "Введите значение в поле Описание товара";
+        }
+
+        //productPriceValidation
+        if (product.getPrice() == null || product.getPrice() == 0) {
+            return "Введите значение в поле цена, отличное от 0";
+        }
+
+        if (file != null && !file.isEmpty()) {
+            File uploadFolder = new File(uploadPath);
+
+            if (!uploadFolder.exists()) {
+                uploadFolder.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile +"."+file.getOriginalFilename();
+
+            try {
+                file.transferTo(new File(uploadPath + "/" + resultFileName));
+            } catch (IOException ex) {
+                return "Что-то не так с файлом";
+            }
+            product.setImageSource(resultFileName);
+        } else {
+            return "Файл не выбран";
+        }
+
+
+        productRepository.save(product);
+        return "OK";
+    }
 }
